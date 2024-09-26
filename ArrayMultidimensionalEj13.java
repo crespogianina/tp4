0d3cb24aca6506d21bc585ecfca05145e5543de0
coti-at <conialtamira9@gmail.com>
2024-09-26 15:30:42 -0300

    Add files via upload

---
+++ ArrayMultidimensionalEj13.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author conit
+ */
+public class ArrayMultidimensionalEj3 {
+
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+        
+        
+    }
+}
